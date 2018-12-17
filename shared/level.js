bbe3b51fc8f5748f56302600603fcee5b9f41258
Marko Mlinaric <markomlinaric1989@gmail.com>
2018-12-17 13:31:46 +0100

    reduce member accesses in movePlayer

--- shared/level.js
+++ shared/level.js
@@ -12,12 +12,14 @@ module.exports = {
     },
 
     movePlayer: function (level, playerID, movement) {
-        level.players[playerID].position.x += movement.x;
-        level.players[playerID].position.y += movement.y;
+        const playerPosition = level.players[playerID].position;
 
-        level.players[playerID].position.x = Math.min(level.world.dimensions.x / 2, level.players[playerID].position.x);
-        level.players[playerID].position.x = Math.max(-level.world.dimensions.x / 2, level.players[playerID].position.x);
-        level.players[playerID].position.y = Math.min(level.world.dimensions.y / 2, level.players[playerID].position.y);
-        level.players[playerID].position.y = Math.max(-level.world.dimensions.y / 2, level.players[playerID].position.y);
+        playerPosition.x += movement.x;
+        playerPosition.y += movement.y;
+
+        playerPosition.x = Math.min(level.world.dimensions.x / 2, playerPosition.x);
+        playerPosition.x = Math.max(-level.world.dimensions.x / 2, playerPosition.x);
+        playerPosition.y = Math.min(level.world.dimensions.y / 2, playerPosition.y);
+        playerPosition.y = Math.max(-level.world.dimensions.y / 2, playerPosition.y);
     }
-}
+};
